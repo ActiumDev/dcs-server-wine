15f93f238b76f5db4e0e3c1b1ff9f3f11c3eb22d
Actium <51276020+ActiumDev@users.noreply.github.com>
2025-05-18 13:18:04 +0000

    shrink idle.exe by reducing PE section alignment to 16 bytes

--- .wine/drive_c/bin/idle.c
+++ .wine/drive_c/bin/idle.c
@@ -1,4 +1,4 @@
-// x86_64-w64-mingw32-gcc -pipe -Wall -mconsole -Os -s -nostdlib -nolibc -nostartfiles -ffreestanding -ffunction-sections -fdata-sections -Wl,--file-alignment=512,--gc-sections,--print-gc-sections idle.c -o idle -lkernel32
+// x86_64-w64-mingw32-gcc -pipe -Wall -mconsole -Os -nostdlib -nolibc -nostartfiles -ffreestanding -ffunction-sections -fdata-sections -Wl,--file-alignment=16,--section-alignment=16,--gc-sections,--strip-all idle.c -o idle -lkernel32
 
 #include <windows.h>
 
@@ -6,5 +6,4 @@ void _start(void) {
 	while (1) {
 		Sleep(MAXDWORD);
 	}
-	ExitProcess(0);
 }
