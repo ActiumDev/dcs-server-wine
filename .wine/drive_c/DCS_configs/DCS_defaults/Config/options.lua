ee7508aa2c2643fc20f5db7e5bc308c309596fc0
Actium <51276020+ActiumDev@users.noreply.github.com>
2025-09-28 13:00:12 +0000

    add default Tacview config options

--- .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
+++ .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
@@ -7,6 +7,16 @@ options = {
 	["plugins"] = {
 		["Tacview"] = {
 			["tacviewExportPath"] = "C:\\DCS_configs\\Tacview",
+			["tacviewFlightDataRecordingEnabled"] = true,
+			["tacviewModuleEnabled"] = true,
+			["tacviewMultiplayerFlightsAsHost"] = 2,
+			["tacviewPlaybackDelay"] = 600,
+			["tacviewRealTimeTelemetryEnabled"] = false,
+			["tacviewRealTimeTelemetryPassword"] = "",
+			["tacviewRealTimeTelemetryPort"] = "42674",
+			["tacviewRemoteControlEnabled"] = false,
+			["tacviewRemoteControlPassword"] = "",
+			["tacviewRemoteControlPort"] = "42675",
 		},
 	},
 }
