c485b747285c485b9445e88e85d103da127d47ac
Actium <51276020+ActiumDev@users.noreply.github.com>
2025-09-28 13:12:49 +0000

    add TODO regarding tacviewExportPath relative path support

--- .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
+++ .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
@@ -6,6 +6,11 @@ options = {
 	},
 	["plugins"] = {
 		["Tacview"] = {
+			-- relative tacviewExportPath supported since 1.9.5 beta 15
+			-- TODO: use relative path when released as stable version
+			-- https://www.tacview.net/download/beta/en/
+			-- https://forum.dcs.world/topic/116593-tacview-the-acmi-for-dcs-world-official-thread/page/77/#findComment-5696840
+			-- http://dogsofwarvu.com/forum/index.php/topic,9832.0.html?PHPSESSID=d4q7nh84g7nk9j3fvr66kbkkqe
 			["tacviewExportPath"] = "C:\\DCS_configs\\Tacview",
 			["tacviewFlightDataRecordingEnabled"] = true,
 			["tacviewModuleEnabled"] = true,
