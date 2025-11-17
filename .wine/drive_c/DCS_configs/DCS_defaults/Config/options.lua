a86118fa70e505fa5489473e8ba507cd2cd4a5ed
Actium <51276020+ActiumDev@users.noreply.github.com>
2025-11-17 17:15:54 +0000

    remove workarounds for per-writedir tacviewExportPath required with Tacview up to v1.9.4

--- .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
+++ .wine/drive_c/DCS_configs/DCS_defaults/Config/options.lua
@@ -6,12 +6,7 @@ options = {
 	},
 	["plugins"] = {
 		["Tacview"] = {
-			-- relative tacviewExportPath supported since 1.9.5 beta 15
-			-- TODO: use relative path when released as stable version
-			-- https://www.tacview.net/download/beta/en/
-			-- https://forum.dcs.world/topic/116593-tacview-the-acmi-for-dcs-world-official-thread/page/77/#findComment-5696840
-			-- http://dogsofwarvu.com/forum/index.php/topic,9832.0.html?PHPSESSID=d4q7nh84g7nk9j3fvr66kbkkqe
-			["tacviewExportPath"] = "C:\\DCS_configs\\Tacview",
+			["tacviewExportPath"] = "Tacview", -- relative to lfs.writedir()
 			["tacviewFlightDataRecordingEnabled"] = true,
 			["tacviewModuleEnabled"] = true,
 			["tacviewMultiplayerFlightsAsHost"] = 2,
